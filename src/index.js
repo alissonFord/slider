efe8174435b13c1cfca83f9ff5020cf87664581f
alissonFord <56479902+alissonFord@users.noreply.github.com>
2020-04-27 19:24:46 +0200

    Adding prototype

--- src/index.js
+++ src/index.js
@@ -3,49 +3,46 @@ function Slider(slider) {
     throw new Error('No slider passed in!');
   }
 
-  // variables for working iwth the slider
-  let current;
-  let prev;
-  let next; 
-
+  this.slider = slider;
   // select the elements needed for the slider
-  const slides = slider.querySelector('.slides');
+  this.slides = slider.querySelector('.slides');
+  // buttons are not used anywhere outside
   const prevButton = slider.querySelector('.goToPrev')
   const nextButton = slider.querySelector('.goToNext');
 
-  function startSlider() {
-    current = slider.querySelector('.current') || slides.firstElementChild;
-    prev = current.previousElementSibling || slides.lastElementChild;
-    next = current.nextElementSibling || slides.firstElementChild;
-  }
-
-  function applyClasses() {
-    current.classList.add('current');
-    prev.classList.add('prev');
-    next.classList.add('next');
-  }
-
-  function move(direction) {
-    const classesToRemove = ['prev', 'current', 'next'];
-    // [prev, current, next].classList.remove(...classesToRemove);
-    prev.classList.remove(...classesToRemove);
-    current.classList.remove(...classesToRemove);
-    next.classList.remove(...classesToRemove);
-    if(direction === 'back') {
-      [prev, current, next] = [prev.previousElementSibling || slides.lastElementChild, prev, current];
-    }
-    else {
-      [prev, current, next] = [current, next, next.nextElementSibling || slides.firstElementChild];
-    }
-    applyClasses();
-  }
-
-  startSlider();
-  applyClasses();
+  this.startSlider();
+  this.applyClasses();
   // if we want to pass an argument to a function, use an arrow function
-  prevButton.addEventListener('click', () => move('back'));
-  nextButton.addEventListener('click', move);
+  prevButton.addEventListener('click', () => this.move('back'));
+  nextButton.addEventListener('click', () => this.move());
+}
+
+Slider.prototype.startSlider = function() {
+  this.current = this.slider.querySelector('.current') || this.slides.firstElementChild;
+  this.prev = this.current.previousElementSibling || this.slides.lastElementChild;
+  this.next = this.current.nextElementSibling || this.slides.firstElementChild;
+}
+
+Slider.prototype.applyClasses = function() {
+  this.current.classList.add('current');
+  this.prev.classList.add('prev');
+  this.next.classList.add('next');
+}
+
+Slider.prototype.move = function(direction) {
+  const classesToRemove = ['prev', 'current', 'next'];
+  // [prev, current, next].classList.remove(...classesToRemove);
+  this.prev.classList.remove(...classesToRemove);
+  this.current.classList.remove(...classesToRemove);
+  this.next.classList.remove(...classesToRemove);
+  if(direction === 'back') {
+    [this.prev, this.current, this.next] = [this.prev.previousElementSibling || this.slides.lastElementChild, this.prev, this.current];
+  }
+  else {
+    [this.prev, this.current, this.next] = [this.current, this.next, this.next.nextElementSibling || this.slides.firstElementChild];
+  }
+  this.applyClasses();
 }
 
 const slider = new Slider(document.querySelector('.slider'));
-const dogSlider = Slider(document.querySelector('.dog-slider'));
+const dogSlider = new Slider(document.querySelector('.dog-slider'));
